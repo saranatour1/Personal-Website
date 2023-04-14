3e642dc02599babeb9c75e7449fb3fcf72d605f6
Sara <saranatourcontact@gmail.com>
2023-04-14 13:40:42 +0300

    fixed top nav-bar

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',views.main), #the main page at route '/' 
     path('contact',views.contact_me), #contactme at route /contact
-    
+    path('about',views.about_me), #aboutme at route /about
+    path('blog',views.blog),
+    path('quote',views.quote),
+    path('gallery',views.gallery), 
 ]
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -6,15 +6,30 @@ from django.shortcuts import render
 
 # For now I have nothing to store in a database
 def main(request):
+  # print(request) #<WSGIRequest: GET '/'>
   return render(request,'main.html')
 
 
 # Contact me route, remember to add form information here? use mailchimp
 
 def contact_me(request):
+
   return render(request,'contactme.html') 
 
 
 # About me  route @ /about
-def contact_me(request):
-  return render(request,'contactme.html') 
+def about_me(request):
+  return render(request,'aboutme.html') 
+
+# blog page @ /blog /keep the blogs here?
+def blog(request):
+  return render(request,'blogs.html')
+
+# Motivitational quotes @quotes
+def quote(request):
+  return render(request,'quote.html')
+
+# @gallery
+
+def gallery(request):
+  return render(request,'gallery.html')
