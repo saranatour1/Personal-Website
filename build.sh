b15c76853a9bfdb396057d859f95354fddab367d
Sara <saranatourcontact@gmail.com>
2023-04-16 00:08:46 +0300

    shell

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
-pip install -r requirements.txt 
-python manage.py collectstatic --no-input
-# python3 manage.py migrate
+#!/bin/bash
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
