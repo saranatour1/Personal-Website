ecf3eeb35f33b037a85e8d2e451842d5915a3873
Sara <saranatourcontact@gmail.com>
2023-08-02 01:02:08 +0300

    Fixes on slider movement

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,6 +1,22 @@
 <script>
-  export let title ; /** this refers to the titles*/
-  export let caption; /** this refers to the small caption beaneath each card*/
-  export let cta; /** This refers to any links in the card*/
-  export let affiliatedWith; /**Is it personal? is it done with other people? */
+  export let item; /** refers to any one of the Items in data.json*/
+
 </script>
+
+<div class="  shadow-lg p-6 h-full w-full m-4 rounded-2xl  mt-2 mb-2 bg-[#1D2939]">
+  <hgroup class="p-4 rounded-t-xl">
+    <h3 class="font-title text-[#FCFCFD] text-xl md:text-2xl pt-2 pb-2">{item}</h3>
+    <p class="text-[#E4E7EC]">Card content goes here. You can add more text or any other elements inside this section.</p>
+
+  </hgroup>
+
+  <div class="w-full  mt-2 overflow-hidden">
+    <span class="w-full h-fit flex justify-around items-center p-3">
+      <p class="text-[#E4E7EC] flex-1 mt-1 flex items-center justify-around">
+        <a href="#" class=" text-blue-600 font-medium hover:underline">Learn More</a>
+        <button class=" bg-blue-600 text-white font-medium px-4 py-2 rounded-md hover:bg-blue-700 ">Action</button>
+      </p>
+    </span>
+  </div>
+
+</div>
--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -1,20 +1,20 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount, onDestroy } from 'svelte';
   import { spring } from 'svelte/motion';
+  import Card from './Card.svelte';
 
   let count = 0;
   const displayed_count = spring();
   $: displayed_count.set(count);
   $: offset = modulo($displayed_count, 1);
-  let interval = 10000;
-  let test = [1,2,3,4,5,6,7,8,9,10]
+  let interval = 15000;
+  let test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let intervalId;
 
   function modulo(n, m) {
-    // handle negative numbers
     return ((n % m) + m) % m;
   }
 
-  // Function to move the things to the right
   const moveRight = () => {
     count = (count + 1) % test.length;
   }
@@ -23,33 +23,42 @@
     count = (count - 1 + test.length) % test.length;
   }
 
-  // Marque style
-  const changeIdx = () => {
-    setInterval(moveRight, interval);
+  const startInterval = () => {
+    intervalId = setInterval(moveRight, interval);
+  }
+
+  const stopInterval = () => {
+    clearInterval(intervalId);
   }
 
   onMount(() => {
-    changeIdx();
-  })
+    startInterval();
+  });
+
+  onDestroy(() => {
+    stopInterval();
+  });
 
   $: console.log(count);
 </script>
 
-<div class="slider">
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="slider" on:mouseenter={stopInterval} on:mouseleave={startInterval} >
   <button id="left" on:click={moveLeft} aria-label="Move left">
     «
   </button>
   <div class="slider-viewport">
-    <div class="slider-digits" style="transform: translate({100 * offset}%,0)">
-      <!-- <strong class="hidden" aria-hidden="true">{test[(count - 1 + test.length) % test.length]}</strong> -->
-      <strong class="text-black text-3xl">{test[count]}</strong>
-      <!-- <strong class="hidden" aria-hidden="true">{test[(count + 1) % test.length]}</strong> -->
+    <div class="slider-digits" style="transform: translate({100 * offset}%, 0)">
+      <Card item={test[count]} />
     </div>
   </div>
   <button id="right" on:click={moveRight} aria-label="Move right">
     »
   </button>
 </div>
+
+
+
 
 <style>
   .slider {
@@ -59,8 +68,8 @@
     border: 1px solid #ccc;
     border-radius: 5px;
     overflow: hidden;
-    width: 200px;
-    height: 40px;
+    width: 100%;
+    height: 100%;
     margin: 0 auto;
     position: relative;
   }
@@ -82,37 +91,18 @@
 
   .slider-viewport {
     width: 100%;
-    height: 100%;
+    height: auto;
     overflow: hidden;
     position: relative;
   }
 
   .slider-digits {
-    position: absolute;
-    top: -41px;
-    left: 0;
-    width: 100%;
-    height: 300%;
+
     display: flex;
     align-items: center;
     justify-content: center;
     transition: transform 1s ease;
   }
 
-  .slider-digits strong {
-    display: block;
-    font-size: 22px;
-    /* color: #fff; */
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    justify-content: center;
-  }
 
-  .slider-digits strong:nth-child(1),
-  .slider-digits strong:nth-child(3) {
-    opacity: 0.5;
-  }
 </style>
