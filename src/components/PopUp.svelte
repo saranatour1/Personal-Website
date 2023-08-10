8249988846dcc0a959e2417b654902bb790fac10
Sara <saranatourcontact@gmail.com>
2023-08-10 15:07:25 +0300

    added Email To Contact Info

---
+++ src/components/PopUp.svelte
@@ -0,0 +1,23 @@
+<!-- Popup to make people star this repo -->
+<script>
+
+  	import Modal from './Modal.svelte';
+		let firstTime = true;
+		$: showModal = true;
+
+
+
+		
+	
+
+			$:console.log(showModal, firstTime)
+
+
+	</script>
+
+		{#if firstTime && showModal}
+			<Modal {showModal} />
+		{/if}
+
+
+
