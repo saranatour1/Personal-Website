e39f533e94f2e39bf831d82e6a9b35228edefb62
Sara <saranatourcontact@gmail.com>
2023-10-02 22:00:09 +0300

    fix: added ts-ignore to allow build

--- src/Custom hooks/useScreenWidth.ts
+++ src/Custom hooks/useScreenWidth.ts
@@ -1,10 +1,16 @@
 import { useSignal , $, useOnWindow } from "@builder.io/qwik";
 
+/**
+ * @ignore
+ * @returns {number} inner screen width 
+ */
+
 export function useScreenWidth() {
   const innerWidth = useSignal<number>();
   useOnWindow(
     'resize',
     $((e) => {
+      //ts-ignore
       innerWidth.value = (e.target as EventTarget).innerWidth;
     })
   );
