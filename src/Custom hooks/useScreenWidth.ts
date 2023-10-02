4be9ddde2e8804e452292875318ed2843ffa2e77
Sara <saranatourcontact@gmail.com>
2023-10-02 22:08:40 +0300

    feat: removed unused screenwidth hook

--- src/Custom hooks/useScreenWidth.ts
+++ src/Custom hooks/useScreenWidth.ts
@@ -1,18 +1,18 @@
-import { useSignal , $, useOnWindow } from "@builder.io/qwik";
+// import { useSignal , $, useOnWindow } from "@builder.io/qwik";
 
-/**
- * @ignore
- * @returns {number} inner screen width 
- */
+// /**
+//  * @ignore
+//  * @returns {number} inner screen width 
+//  */
 
-export function useScreenWidth() {
-  const innerWidth = useSignal<number>();
-  useOnWindow(
-    'resize',
-    $((e) => {
-      //ts-ignore
-      innerWidth.value = (e.target as EventTarget).innerWidth;
-    })
-  );
-  return innerWidth;
-}
+// // export function useScreenWidth() {
+// //   const innerWidth = useSignal<number>();
+// //   useOnWindow(
+// //     'resize',
+// //     $((e) => {
+// //       //ts-ignore
+// //       innerWidth.value = (e.target as EventTarget).innerWidth;
+// //     })
+// //   );
+// //   return innerWidth;
+// // }
