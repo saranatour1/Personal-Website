6e61c622c5a8be02df5c068b7e1f93805ff104af
saranatour1 <saranatourcontact@gmail.com>
2024-01-15 17:26:26 +0200

    fixed transition issue in styles

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -6,9 +6,9 @@ import { usePathname } from 'next/navigation'
 function NavigationBar() {
   const pathName = usePathname();
   return (
-    <nav className="w-full grid h-full max-w-[31.75rem] max-h-20 grid-cols-5 grid-rows-1 self-center my-4 bg-transparent backdrop-blur-md ">
+    <nav className="w-full grid h-full max-w-[31.75rem] max-h-14 grid-cols-5 grid-rows-1 self-center my-4 bg-transparent backdrop-blur-md ">
       {routes.map(([routeName, path], index) => (
-        <Link key={index} href={path} className={`text-center text-green-950 text-base flex items-center justify-center gap-2 capitalize w-full h-10 max-w-[6.3125rem]  max-h-[4rem] rounded-full bg-transparent transition-colors   duration-150 origin-left ${ path === pathName ? ' bg-indigo-100 origin-center':'' }`} >
+        <Link key={index} href={path} className={`text-center text-green-950 text-base flex items-center justify-center gap-2 capitalize w-full h-full max-w-[6.3125rem]  max-h-[3rem] rounded-full  transition-colors duration-150 origin-left ${ path === pathName ? 'bg-green-950/10 origin-center':'' }`} >
           {routeName}
           <span className={`grid place-content-center leading-[18px] text-[calc(.875rem-2px)] justify-center items-center border border-gray-900 w-6 h-6 rounded-md ml-2 ${pathName === path ? 'opacity-100':'opacity-0'} transition-opacity`}>\</span>
         </Link>
