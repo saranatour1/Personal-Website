3b9cc1fc38de9a566b060b9d9e47f56aaf3f9b82
saranatour1 <saranatourcontact@gmail.com>
2024-01-27 01:40:39 +0200

    update tailwind styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'base-grey':"#ABB2BF",
+				'tag-purple':'#C778DD',
+				'base-black':"#282C33",
+			},
+			fontFamily:{
+				'serif4':['Source Serif 4', 'serif'],
+				'lora':['Lora', 'serif'],
+				'code':['Fira Code', 'monospace']
+			}
+		},
 	},
 	plugins: [],
 }
