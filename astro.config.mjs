bfa57d4091fd403e7195e823d14f394b44847327
saranatour1 <saranatourcontact@gmail.com>
2024-01-29 17:14:08 +0200

    updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site:'https://www.saranatour.dev/',
+  integrations: [tailwind(), sitemap({
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date('2024-01-29'),
+    i18n: {
+      defaultLocale: 'en', 
+      locales: {
+        en: 'en-US', 
+        ar:'ar-AE'
+      },
+    },
+  })]
 });
