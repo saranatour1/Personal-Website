4d255698f57ccc2126818ad1aeb37787273ebf73
saranatour1 <saranatourcontact@gmail.com>
2024-02-01 23:05:42 +0200

    updates on meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Sara Natour's Personal website";
+export const SITE_DESCRIPTION = "The personal Website of Sara Natour.";
 export const socialLinks = [
   import.meta.env.PUBLIC_EMAIL,
   import.meta.env.PUBLIC_LINKEDIN,
