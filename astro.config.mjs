34ae89326dac9ec24d178c1e28d11fbe5907805f
saranatour1 <saranatourcontact@gmail.com>
2024-02-11 10:07:00 +0200

    TAS-50/ Chore: updated domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.saranatour.dev/',
+  site: 'https://saranatour.dev/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
