fbb5f0efed35b380dba294ac746d034274343dd4
Sara <77834808+saranatour1@users.noreply.github.com>
2024-04-26 00:14:22 +0300

    Latest updates (#47)
    * deleted un-used files
    
    * added oktakit
    
    * Add skills, projects, and overview sections; include squiggly lines for decoration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,17 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        yellow: {
-          5: "#F0DBA5",
-          10: "#EDB74D",
+        "white-1":"#fcf8ef", // bg
+        "black-1":"#1a1a1a",
+        "blue-1":"#2a9d8f",
         },
-        red: {
-          5: "#EB6666",
-        },
-        green: {
-          5: "#6FB18A",
-        },
-      },
       fontFamily: {
         recoleta: ["Recoleta"],
       },
