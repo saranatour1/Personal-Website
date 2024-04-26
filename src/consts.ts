736b408e3b2d9ac1f1274fa065415c849295adb1
saranatour1 <saranatourcontact@gmail.com>
2024-04-26 19:14:56 +0300

    updated constants

--- src/consts.ts
+++ src/consts.ts
@@ -7,3 +7,81 @@ export const socialLinks = [
   import.meta.env.PUBLIC_LINKEDIN,
   import.meta.env.PUBLIC_GITHUB,
 ] as string[];
+
+export const skillSet = [
+  {
+    name: 'JavaScript',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+  },
+  {
+    name: 'TypeScript',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg',
+    link: 'https://www.typescriptlang.org/'
+  },
+  {
+    name: 'Python',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg',
+    link: 'https://www.python.org/'
+  },
+  {
+    name: 'Java',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/java-colored.svg',
+    link: 'https://www.oracle.com/java/'
+  },
+  {
+    name: 'Git',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg',
+    link: 'https://git-scm.com/'
+  },
+  {
+    name: 'React',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg',
+    link: 'https://reactjs.org/'
+  },
+  {
+    name: 'TailwindCSS',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg',
+    link: 'https://tailwindcss.com/'
+  },
+  {
+    name: 'Bootstrap',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg',
+    link: 'https://getbootstrap.com/'
+  },
+  {
+    name: 'NodeJS',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg',
+    link: 'https://nodejs.org/en/'
+  },
+  {
+    name: 'MongoDB',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg',
+    link: 'https://www.mongodb.com/'
+  },
+  {
+    name: 'MySQL',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg',
+    link: 'https://www.mysql.com/'
+  },
+  {
+    name: 'Flask',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/flask-colored.svg',
+    link: 'https://flask.palletsprojects.com/en/2.0.x/'
+  },
+  {
+    name: 'Django',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/django-colored.svg',
+    link: 'https://www.djangoproject.com/'
+  },
+  {
+    name: 'Svelte',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/svelte-colored.svg',
+    link: 'https://svelte.dev/'
+  },
+  {
+    name: 'VueJS',
+    icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vuejs-colored.svg',
+    link: 'https://vuejs.org/'
+  },
+];
