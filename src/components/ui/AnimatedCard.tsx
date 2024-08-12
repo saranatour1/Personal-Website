23e257af54822fc90f6c6147559b4143c1707579
Sara <77834808+saranatour1@users.noreply.github.com>
2024-08-12 16:56:04 +0300

    update performance  (#49)
    * added Aceternity UI
    
    * updated animation
    
    * update performance

---
+++ src/components/ui/AnimatedCard.tsx
@@ -0,0 +1,189 @@
+import { animate, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { cn } from "../../lib/utils";
+import { GoCopilot } from "react-icons/go";
+export const AnimatedCard = ()=>{
+  return ( <Card>
+    <CardSkeletonContainer>
+      <Skeleton />
+    </CardSkeletonContainer>
+    <CardTitle>Damn good card</CardTitle>
+    <CardDescription>
+      A card that showcases a set of tools that you use to create your
+      product.
+    </CardDescription>
+  </Card>)
+}
+
+const Skeleton = () => {
+  const scale = [1, 1.1, 1];
+  const transform = ["translateY(0px)", "translateY(-4px)", "translateY(0px)"];
+  const sequence = [
+    [
+      ".circle-1",
+      {
+        scale,
+        transform,
+      },
+      { duration: 0.8 },
+    ],
+    [
+      ".circle-2",
+      {
+        scale,
+        transform,
+      },
+      { duration: 0.8 },
+    ],
+    [
+      ".circle-3",
+      {
+        scale,
+        transform,
+      },
+      { duration: 0.8 },
+    ],
+    [
+      ".circle-4",
+      {
+        scale,
+        transform,
+      },
+      { duration: 0.8 },
+    ],
+    [
+      ".circle-5",
+      {
+        scale,
+        transform,
+      },
+      { duration: 0.8 },
+    ],
+  ];
+
+  useEffect(() => {
+    // @ts-ignore
+    animate(sequence, {
+      repeat: Infinity,
+      repeatDelay: 1,
+    });
+  }, []);
+  return (
+    <div className="p-8 overflow-hidden h-full relative flex items-center justify-center">
+      <div className="flex flex-row flex-shrink-0 justify-center items-center gap-2">
+      <Container>
+        s
+      </Container>
+      </div>
+
+      <div className="h-40 w-px absolute top-20 m-auto z-40 bg-gradient-to-b from-transparent via-cyan-500 to-transparent animate-move">
+        <div className="w-10 h-32 top-1/2 -translate-y-1/2 absolute -left-10">
+          {/* <Sparkles /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "max-w-full w-full mx-auto  rounded-xl border border-[rgba(255,255,255,0.10)] bg-gray-100 shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] group",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const CardTitle = ({
+  children,
+  className,
+  id
+}: {
+  children: React.ReactNode;
+  className?: string;
+  id?:string;
+}) => {
+  return (
+    <h3
+      id={id}
+      className={cn(
+        "text-lg font-semibold text-gray-800 py-2",
+        className
+      )}
+    >
+      {children}
+    </h3>
+  );
+};
+
+export const CardDescription = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <p
+      className={cn(
+        "text-sm font-normal text-neutral-600 max-w-full",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
+export const CardSkeletonContainer = ({
+  className,
+  children,
+  showGradient = true,
+}: {
+  className?: string;
+  children: React.ReactNode;
+  showGradient?: boolean;
+}) => {
+  return (
+    <div
+      className={cn(
+        "h-[15rem] md:h-[20rem] rounded-xl z-40",
+        className,
+        showGradient &&
+          "bg-neutral-300 [mask-image:radial-gradient(50%_50%_at_50%_50%,white_0%,transparent_100%)]"
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Container = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        `h-16 w-16 rounded-full flex items-center justify-center bg-[rgba(248,248,248,0.01)]
+    shadow-[0px_0px_8px_0px_rgba(248,248,248,0.25)_inset,0px_32px_24px_-16px_rgba(0,0,0,0.40)]
+    `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
--- src/consts.ts
+++ src/consts.ts
@@ -12,76 +12,76 @@ export const skillSet = [
   {
     name: 'JavaScript',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg',
-    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
   },
   {
     name: 'TypeScript',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg',
-    link: 'https://www.typescriptlang.org/'
+    url: 'https://www.typescriptlang.org/'
   },
   {
     name: 'Python',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg',
-    link: 'https://www.python.org/'
+    url: 'https://www.python.org/'
   },
   {
     name: 'Java',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/java-colored.svg',
-    link: 'https://www.oracle.com/java/'
+    url: 'https://www.oracle.com/java/'
   },
   {
     name: 'Git',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg',
-    link: 'https://git-scm.com/'
+    url: 'https://git-scm.com/'
   },
   {
     name: 'React',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg',
-    link: 'https://reactjs.org/'
+    url: 'https://reactjs.org/'
   },
   {
     name: 'TailwindCSS',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg',
-    link: 'https://tailwindcss.com/'
+    url: 'https://tailwindcss.com/'
   },
   {
     name: 'Bootstrap',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg',
-    link: 'https://getbootstrap.com/'
+    url: 'https://getbootstrap.com/'
   },
   {
     name: 'NodeJS',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg',
-    link: 'https://nodejs.org/en/'
+    url: 'https://nodejs.org/en/'
   },
   {
     name: 'MongoDB',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg',
-    link: 'https://www.mongodb.com/'
+    url: 'https://www.mongodb.com/'
   },
   {
     name: 'MySQL',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg',
-    link: 'https://www.mysql.com/'
+    url: 'https://www.mysql.com/'
   },
   {
     name: 'Flask',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/flask-colored.svg',
-    link: 'https://flask.palletsprojects.com/en/2.0.x/'
+    url: 'https://flask.palletsprojects.com/en/2.0.x/'
   },
   {
     name: 'Django',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/django-colored.svg',
-    link: 'https://www.djangoproject.com/'
+    url: 'https://www.djangoproject.com/'
   },
   {
     name: 'Svelte',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/svelte-colored.svg',
-    link: 'https://svelte.dev/'
+    url: 'https://svelte.dev/'
   },
   {
     name: 'VueJS',
     icon: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vuejs-colored.svg',
-    link: 'https://vuejs.org/'
+    url: 'https://vuejs.org/'
   },
 ];
